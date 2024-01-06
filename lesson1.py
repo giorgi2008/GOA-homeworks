2e68db8f97f6349af8f3c7e82eb246fb875ccdee
giorgi2008 <gqsovreli88@gmail.com>
2024-01-06 09:38:26 -0800

    first commit

---
+++ lesson1.py
@@ -0,0 +1,15 @@
+print("gamarjoba")
+print("me var")
+name = "giorgi"
+surname = "qsovreli"
+print (name + " " + surname)
+print ("simarleshi daaxloebit var")
+height = "1.79"
+print (height)
+print("var")
+age = "14"
+print (str(age))
+print("wlis")
+print("miyvars")
+favorite_sport = "fexburti"
+print (favorite_sport)
